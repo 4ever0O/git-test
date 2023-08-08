4c50f92bd04b6699cacc9b015bf4d3c9058864d1
Zjm <2805779247@qq.com>
2023-08-08 09:47:07 +0800

    error

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,6 @@ public class GitTest {
         System.out.println("gitee test!");
         System.out.println("gitee test222!");
         System.out.println("2023================");
-        System.out.printIn("pull test");
+        System.out.println("pull test");
     }
 }
